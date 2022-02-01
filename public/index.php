f2b2e64c7944702f6e1a3d58a73ade036df5ebbd
Dmytro_Parkhomenko <dmytro.parkhomenko@geocomply.com>
2022-02-01 19:06:12 +0200

    implemented layout view.

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\core\Application;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = new Application(dirname(__DIR__));
+
+
+$app->router->get('/', 'home');
+
+$app->router->get('/contact', 'contact');
+
+$app->run();
