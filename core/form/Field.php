1748d39d904439c9834f6ed0951090331925dc43
Dmytro_Parkhomenko <dmytro.parkhomenko@geocomply.com>
2022-02-02 13:27:16 +0200

    Implemented validation for the registration.

---
+++ core/form/Field.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\core\form;
+
+use App\core\Model;
+
+class Field
+{
+    public const TYPE_TEXT = 'text';
+    public const TYPE_EMAIL = 'email';
+    public const TYPE_PASSWORD = 'password';
+    public const TYPE_CONFIRM_PASSWORD = 'password';
+
+
+    public Model $model;
+    public string $attribute;
+    public string $type;
+
+    public function __construct(Model $model, string $attribute)
+    {
+        $this->type = self::TYPE_TEXT;
+        $this->model = $model;
+        $this->attribute = $attribute;
+    }
+
+    public function __toString(): string
+    {
+        return sprintf('
+                <div class="mb-3">
+                    <label class="form-label">%s</label>
+                    <input type="%s" name="%s" value="%s" class="form-control%s">
+                    <div class="invalid-feedback">
+                    %s
+                    </div>
+                </div>
+            ',
+            ucfirst($this->attribute),
+            $this->type,
+            $this->attribute,
+            $this->model->{$this->attribute},
+            $this->model->hasError($this->attribute) ? ' is-invalid' : '',
+            $this->model->getFirstError($this->attribute)
+        );
+    }
+
+    public function typeField($attribute)
+    {
+        if ($attribute === self::TYPE_PASSWORD || $attribute === 'confirmPassword') {
+            $this->type = self::TYPE_PASSWORD;
+        } elseif ($attribute === self::TYPE_EMAIL) {
+            $this->type = self::TYPE_EMAIL;
+        }
+        return $this;
+    }
+}
--- views/register.php
+++ views/register.php
@@ -1,31 +1,18 @@
 <h1>Registration</h1>
+<?php use App\core\form\Form;
 
-<form action="" method="post">
-    <div class="row">
-        <div class="col">
-            <div class="mb-3">
-                <label class="form-label">Firstname</label>
-                <input type="text" name="firstname" class="form-control">
-            </div>
-        </div>
-        <div class="col">
-            <div class="mb-3">
-                <label class="form-label">Lastname</label>
-                <input type="text" name="lastname" class="form-control">
-            </div>
-        </div>
+ $form = Form::begin('', 'post')?>
+<div class="row">
+    <div class="col">
+        <?php echo $form->field($model, 'firstname') ?>
     </div>
-    <div class="mb-3">
-        <label class="form-label">Email</label>
-        <input type="email" name="email" class="form-control">
+    <div class="col">
+        <?php echo $form->field($model, 'lastname') ?>
     </div>
-    <div class="mb-3">
-        <label class="form-label">Password</label>
-        <input type="password" name="password" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label class="form-label">Confirm password</label>
-        <input type="password" name="confirmPassword" class="form-control">
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+</div>
+<?php echo $form->field($model, 'email')->typeField('email') ?>
+<?php echo $form->field($model, 'password')->typeField('password') ?>
+<?php echo $form->field($model, 'confirmPassword')->typeField('confirmPassword') ?>
+<button type="submit" class="btn btn-primary">Submit</button>
+
+<?php Form::end() ?>
